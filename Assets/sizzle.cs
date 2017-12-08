71c70211d5da3c65ed14d70501d47a9673759d02
Noah Warns <32556975+nwarns21@users.noreply.github.com>
2017-12-08 12:27:37 -0500

    lake

---
+++ Assets/sizzle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sizzle : MonoBehaviour {
+	AudioSource saw;
+	// Use this for initialization
+	void Start () {
+		saw = GameObject.FindObjectOfType<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void startGrill() {
+		saw.Play ();
+		Debug.Log ("Nurrrr");
+		saw.loop = true;
+	}
+
+	void OnCollisionEnter(Collision col) {
+
+		if (col.gameObject) {
+			startGrill ();
+		}
+
+	}
+}
